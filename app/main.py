4a2674bd23a237c7e687d8d766fd86c6a16a7b89
Anna S <my@example.com>
2025-09-05 22:06:01 -0300

    solution, pytest flake8 run

--- app/main.py
+++ app/main.py
@@ -1,1 +1,55 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def get_damage(self, damage: int = 50) -> None:
+        self.health -= damage
+        if self.health <= 0:
+            self.health = 0
+            self.die()
+
+    def die(self) -> None:
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+
+    @property
+    def convert_to_dict(self) -> dict:
+        return {
+            "Name": self.name,
+            "Health": self.health,
+            "Hidden": self.hidden
+        }
+
+    def __repr__(self) -> str:
+        result = []
+        dict_result = self.convert_to_dict
+        result.append(
+            f"{{Name: {dict_result["Name"]}, "
+            f"Health: {dict_result["Health"]}, "
+            f"Hidden: {dict_result["Hidden"]}}}"
+        )
+        return f"{",".join(result)}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, target: Animal) -> None:
+        if isinstance(target, Herbivore):
+            if target.hidden:
+                print(f"{self.name} cannot bite hidden {target.name}")
+            else:
+                target.get_damage(50)
